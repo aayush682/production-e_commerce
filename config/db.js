6412494f404e60e6b162ffe1d306cc77c757af95
Aayush Tiwari <tiwariaayush682@gmail.com>
2023-05-22 02:03:28 +0530

    Final Code
    e-commerce mern stack app

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import colors from 'colors'
+import dotenv from 'dotenv'
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected To MongoDB DataBase ${conn.connection.host}`.bgMagenta.white);
+    } catch (error) {
+        console.log(`Error in mondoDB ${error}`.bgRed.white);
+    }
+}
+
+export default connectDB;
